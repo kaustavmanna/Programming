a523d767f7f006d4fa80440fb0e6bd78c384e7e3
Kaustav Manna <54213594+kaustavmanna@users.noreply.github.com>
2019-10-29 22:27:13 +0530

    Update CircularDoubleList.java

--- Data_Structure_Algorithms/src/com/linkedlist/CircularDoubleList.java
+++ Data_Structure_Algorithms/src/com/linkedlist/CircularDoubleList.java
@@ -6,7 +6,26 @@ package com.linkedlist;
  */
 public class CircularDoubleList<E>
 {
+	private DoubleListNode<E> head;
+	
 	public CircularDoubleList()
 	{
+		head = null;
+	}
+	
+	public int size()
+	{
+		int count = 0;
+		if(head != null)
+		{
+			DoubleLinkedList<E> N = head;
+			count++;
+			while(N.getNext() != head)
+			{
+				N = N.getNext();
+				count++;
+			}
+		}
+		return count;
 	}
 }
