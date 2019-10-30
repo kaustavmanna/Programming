bf635dc195b61c9dc79098dc4dda2b8e17b96a42
Kaustav Manna <kaustav.manna23@gmail.com>
2019-10-30 16:34:17 +0530

    Updated LinkedList

--- Data_Structure_Algorithms/src/com/linkedlist/CircularSingleLinkedList.java
+++ Data_Structure_Algorithms/src/com/linkedlist/CircularSingleLinkedList.java
@@ -16,6 +16,7 @@ public class CircularSingleLinkedList<E>
 	public void deletelist()
 	{
 		head = null;
+		System.out.println("The list is deleted!");
 	}
 	
 	public int size()
@@ -72,6 +73,103 @@ public class CircularSingleLinkedList<E>
 	
 	public void insertPosition(int position, E data)
 	{
+		SingleListNode<E> N = head;
+		int i = 1;
+		for(; N.getNext() != head && i <= position; i++)
+			N = N.getNext();
+		if(i != position)
+			System.out.println("Invalid Position Requested!");
+		else
+		{
+			SingleListNode<E> newnode = new SingleListNode<E>(data);
+			N.setNext(newnode);
+			newnode.setNext(head);
+		}
+	}
+	
+	public void deleteNode(E data)
+	{
+		if(head == null)
+		{
+			System.out.println("The list is empty!");
+			return;
+		}
 		
+		SingleListNode<E> N = head;
+		
+		if(N.getData() == data)
+		{
+			while(N.getNext() != head)
+				N = N.getNext();
+			N.setNext(head.getNext());
+			SingleListNode<E> temp = head.getNext();
+			head.setNext(null);
+			head = temp;
+			
+			System.out.println(data + " is deleted from the list!");
+			return;
+		}
+		else
+		{
+			while(N.getNext() != head)
+			{
+				if(N.getNext().getData() == data)
+				{
+					SingleListNode<E> temp = N.getNext();
+					N.setNext(temp.getNext());
+					temp.setNext(null);
+					System.out.println(data + " is deleted from the list!");
+					return;
+				}
+				N = N.getNext();
+			}
+			System.out.println(data + " does not exist in the list!");
+		}
+	}
+	
+	public void deletePosition(int position)
+	{
+		if(head == null)
+		{
+			System.out.println("The list is empty!");
+			return;
+		}
+		
+		else if(position == 1)
+		{
+			E data = head.getData();
+			SingleListNode<E> N = head;
+			
+			while(N.getNext() != head)
+				N = N.getNext();
+			
+			N.setNext(head.getNext());
+			N = head.getNext();
+			head.setNext(null);
+			head = N;
+			System.out.println(data + " is deleted from the list!");
+		}
+		else
+		{
+			int i = 1;
+			SingleListNode<E> N = head;
+			
+			for(; N.getNext() != head && i < position; i++)
+				N = N.getNext();
+			
+			if(i != position)
+			{
+				System.out.println("Invalid position requested!");
+				return;
+			}
+			else
+			{
+				E data = N.getNext().getData();
+				SingleListNode<E> temp = N.getNext();
+				N.setNext(temp.getNext());
+				temp.setNext(null);
+				System.out.println(data + " is deleted from the list!");
+			}
+		}
 	}
 }
--- Data_Structure_Algorithms/src/com/linkedlist/LinkedListImpl.java
+++ Data_Structure_Algorithms/src/com/linkedlist/LinkedListImpl.java
@@ -4,22 +4,17 @@ public class LinkedListImpl
 {
 	public static void main(String[] args)
 	{
-		//SingleLinkedList<Integer> list1 = new SingleLinkedList<Integer> ();
-		//DoubleLinkedList<Integer> list1 = new DoubleLinkedList<Integer> ();
+		CircularSingleLinkedList<Integer> list1 = new CircularSingleLinkedList<Integer>();
 		
-		/*Single Linked List Using Array Test*/
-		SingleLinkedListArray list1 = new SingleLinkedListArray();
+		list1.insertNode(1);
+		//list1.insertNode(3);
+		//list1.insertNode(5);
 		
-		for(int i = 1; i <= 10; i++)
-			list1.insertNode(i*i);
 		list1.traverse();
-		list1.insertPosition(100, 99);
-		list1.traverse();
-		//list1.deletePosition(11);
-		list1.traverse();
-		//list1.deletePosition(5);
-		list1.traverse();
-		list1.insertNode(10);
+		
+		list1.deleteNode(1);
+		//list1.insertNode(7);
+		
 		list1.traverse();
 	}
 }
