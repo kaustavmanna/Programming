de1a591ebdc6b6f57050233e7be67ee901a0a030
Kaustav Manna <kaustav.manna23@gmail.com>
2019-11-05 00:12:28 +0530

    Adding HeapSort

---
+++ Data_Structure_Algorithms/src/com/sort/HeapSort.java
@@ -0,0 +1,36 @@
+/**
+ * @author kaustavmanna
+ *
+ */
+
+package com.sort;
+
+import com.heap.*;
+
+public class HeapSort
+{
+	public static void sort(int arr[], SortOrder order)
+	{
+		if(order == SortOrder.Ascending)
+		{
+			Heap heap = new MinHeap();
+			
+			for(int i = 0; i < arr.length; i++)
+				heap.insert(arr[i]);
+			
+			for(int i = 0; i < arr.length; i++)
+				arr[i] = heap.removeTop();
+		}
+		
+		else
+		{
+			Heap heap = new MaxHeap();
+			
+			for(int i = 0; i < arr.length; i++)
+				heap.insert(arr[i]);
+			
+			for(int i = 0; i < arr.length; i++)
+				arr[i] = heap.removeTop();
+		}
+	}
+}
--- Data_Structure_Algorithms/src/com/sort/SortingImpl.java
+++ Data_Structure_Algorithms/src/com/sort/SortingImpl.java
@@ -9,43 +9,19 @@ public class SortingImpl
 {
 	public static void main(String[] args)
 	{
-		//int arr[] = {1, 10, 2, 9, 3, 8, 4, 7, 5, 6};
-		int arr2[] = {19, 12, 38, 24, 57, 36, 76, 48, 95, 10};
+		int arr[] = {1, 10, 2, 9, 3, 8, 4, 7, 5, 6};
 		
 		/*Before Sorting Array*/
 		System.out.println("Before sorting the array is: ");
-		for(int i = 0; i < arr2.length; i++)
-		{
-			System.out.print(arr2[i] + " ");
-		}
+		for(int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
 		
-		/*Calling Sort Method*/
-		//BubbleSort.sort(arr, SortOrder.Ascending);
-		//BubbleSort.sort(arr, SortOrder.Descending);
-		
-		//SelectionSort.sort(arr, SortOrder.Ascending);
-		//SelectionSort.sort(arr, SortOrder.Descending);
-		
-		//InsertionSort.sort(arr, SortOrder.Ascending);
-		//InsertionSort.sort(arr, SortOrder.Descending);
-		
-		//ShellSort.sort(arr, SortOrder.Ascending);
-		//ShellSort.sort(arr, SortOrder.Descending);
-		
-		//MergeSort.sort(arr, SortOrder.Ascending);
-		//MergeSort.sort(arr, SortOrder.Descending);
-		
-		//QuickSort.sort(arr2, SortOrder.Ascending);
-		//QuickSort.sort(arr2, SortOrder.Descending);
-		
-		//int sorted[] = CountingSort.sort(arr2, 1, 100, SortOrder.Ascending);
-		int sorted[] = CountingSort.sort(arr2, 1, 100, SortOrder.Descending);
+		//Calling sort method
+		HeapSort.sort(arr, SortOrder.Descending);
 		
 		/*Post Sorting Array*/
 		System.out.println("\nAfter sorting the array is: ");
-		for(int i = 0; i < sorted.length; i++)
-		{
-			System.out.print(sorted[i] + " ");
-		}
+		for(int i = 0; i < arr.length; i++)
+			System.out.print(arr[i] + " ");
 	}
 }
